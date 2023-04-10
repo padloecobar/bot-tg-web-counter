d7fdc2b47929a7d4163b2384c44af0099f580321
pat902911 <pavel.koulikou@bellmedia.ca>
2023-04-09 23:05:05 -0400

    init

--- front/vue-app/src/App.vue
+++ front/vue-app/src/App.vue
@@ -1,43 +1,38 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Button from "primevue/button"
+
 </script>
 
 <template>
-  <header>
-    Header
-  </header>
+  <div class="main">
+    <div>
+      HELO WORLD
+    </div>
+    <div>
+      <div class="card flex justify-content-center flex-wrap gap-3">
+        <Button label="Primary" />
+        <Button label="Secondary" severity="secondary" />
+        <Button label="Success" severity="success" />
+        <Button label="Info" severity="info" />
+        <Button label="Warning" severity="warning" />
+        <Button label="Help" severity="help" />
+        <Button label="Danger" severity="danger" />
 
-  <main>
-    HELO WORLD
-  </main>
+      </div>
+      <div class="card">
+        <div class="flex flex-wrap card-container blue-container" style="max-width: 500px">
+          <div class="flex align-items-center justify-content-center bg-blue-500 font-bold text-white m-2 border-round" style="min-width: 200px; min-height: 100px">1</div>
+          <div class="flex align-items-center justify-content-center bg-blue-500 font-bold text-white m-2 border-round" style="min-width: 200px; min-height: 100px">2</div>
+          <div class="flex align-items-center justify-content-center bg-blue-500 font-bold text-white m-2 border-round" style="min-width: 200px; min-height: 100px">3</div>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- front/vue-app/src/main.ts
+++ front/vue-app/src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 
 import './assets/main.css'
+import Button from "primevue/button";
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue);
+
+app.component('Button', Button);
+
 
 app.mount('#app')
